c12086a9e766ae83380b6957138c8add66176690
Sandra Martinez Gutierrez <sandra.martinez@pucp.edu.pe>
2022-07-22 17:55:56 -0500

    mgtecon634_python

---
+++ _build/jupyter_execute/md/intro.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Machine Learning-Based Causal Inference
+# 
+# This JupyterBook has been created based on the tutorials of the course MGTECON 634 at Stanford taught by Professor Susan Athey. All the scripts were in R-markdown and we decided to translate each of them into Python, so students can manage both programing languages. We aim to add more empirical examples were the ML and CI tools can be applied using both programming languages.
+# 
+# 
+# ```{tableofcontents}
+# ```
+# 
+# 
+# ```{admonition} Incoming website
+# This JupyterBook will receive future updates. It's a draft. Not ready for publication.
+# ```
+# 
+# ```{admonition} Repository
+# You can find all of these Python scripts in this [repository](https://github.com/d2cml-ai/mgtecon634_python/tree/main/notebooks). 
+# ```
